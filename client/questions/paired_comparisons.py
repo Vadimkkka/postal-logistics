f2b016c58f7a3d4a9c750648c9a561599b94cf1e
Vadimkkka <vadom99@mail.ru>
2021-05-20 05:32:43 +0300

    Парное сравнение 🤦

---
+++ client/questions/paired_comparisons.py
@@ -0,0 +1,24 @@
+from helpers import is_not_empty
+
+
+q_paired_comparisons = [
+    {
+        'type': 'list',
+        'name': 'Z1',
+        'message': 'Z1',
+        'choices': ['0', '1']
+    },
+    {
+        'type': 'list',
+        'name': 'Z2',
+        'message': 'Z2',
+        'choices': ['0', '1']
+    },
+    {
+        'type': 'list',
+        'name': 'Z3',
+        'message': 'Z3',
+        'choices': ['0', '1']
+    },
+
+]
